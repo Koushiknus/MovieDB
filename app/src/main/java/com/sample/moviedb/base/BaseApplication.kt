527f840c427e9a55fa96024ce1e206e0e1438261
Radhakrisnan Koushik <radhakrisnan.koushik@cartrack.com>
2019-10-11 19:43:11 +0800

    NEW : Dagger Integration

---
+++ app/src/main/java/com/sample/moviedb/base/BaseApplication.kt
@@ -0,0 +1,13 @@
+package com.sample.moviedb.base
+
+import com.sample.moviedb.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class BaseApplication : DaggerApplication() {
+
+
+      override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+         return DaggerAppComponent.builder().application(this).build()
+      }
+}
