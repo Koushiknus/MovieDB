e87e34ef9ae4931cf51c3cde341b39e49bc3d2e1
koushik <koushik>
2019-12-15 12:01:36 +0800

    Update : Unit Test added for Movie List display

---
+++ app/src/test/java/com/sample/moviedb/repository/MovieListRepositoryTest.kt
@@ -0,0 +1,39 @@
+package com.sample.moviedb.repository
+
+import com.sample.moviedb.base.BaseRepository
+import com.sample.moviedb.model.Movie
+import com.sample.moviedb.network.ApiMethods
+import com.sample.moviedb.ui.movieList.MovieListRepository
+import com.sample.moviedb.util.InstantExecutorExtension
+import io.reactivex.observers.TestObserver
+import io.reactivex.schedulers.Schedulers
+import org.junit.Assert
+import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito
+import javax.inject.Inject
+
+@ExtendWith(InstantExecutorExtension::class)
+class MovieListRepositoryTest : BaseRepository() {
+
+
+
+    var movieListRepository : MovieListRepository? = null
+
+
+    @BeforeEach
+    fun initEach() {
+    }
+
+    @Test
+    fun discoverMovies() {
+        movieListRepository = MovieListRepository()
+        val results  = movieListRepository?.discoverMovies(1)
+        val testObserver : TestObserver<ArrayList<Movie>>  =  results!!.test()
+        testObserver.assertSubscribed()
+        testObserver.assertNoErrors()
+        testObserver.assertNoTimeout()
+
+    }
+}
